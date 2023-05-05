ae119fb2a9718ded54277aed2bfabce4015964ba
cbx0915 <caoboxun@outlook.com>
2023-05-05 14:48:45 +0800

    vault backup: 2023-05-05 14:48:44

--- .obsidian/plugins/obsidian-emoji-toolbar/main.js
+++ .obsidian/plugins/obsidian-emoji-toolbar/main.js
@@ -104185,22 +104185,22 @@ const DEF_DELAY = 1000;
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
 }
-function insertext(editor,text) {
-  if (text.length === 0 || text==null) return;
-  const cursor = editor.getCursor('from');
-  editor.replaceRange(text, cursor, cursor);
-   app.commands.executeCommandById("editor:focus");
-   app.workspace.activeLeaf.view.editor.exec("goRight");
+function insertText(editor, text) {
+    if (text.length === 0 || text == null)
+        return;
+    const cursor = editor.getCursor('from');
+    editor.replaceRange(text, cursor, cursor);
+    app.commands.executeCommandById("editor:focus");
+    app.workspace.activeLeaf.view.editor.exec("goRight");
 }
 class EmojiModal extends obsidian.Modal {
-    constructor(app, theme, isNative,editor) {
+    constructor(app, theme, isNative, editor) {
         super(app);
         this.reactComponent = react.createElement(EmojiToolbar, {
             "onSelect": async (emoji) => {
                 this.close();
                 await sleep(10);
-             //   document.execCommand('insertText', false, emoji.native);
-              insertext(editor, emoji.native);
+                insertText(editor, emoji.native);
             },
             "onClose": () => {
                 this.close();
@@ -104240,11 +104240,12 @@ class EmojiPickerPlugin extends obsidian.Plugin {
                     if (!checking) {
                         try {
                             const theme = this.app.getTheme() === 'moonstone' ? 'light' : 'dark';
-                          let view = this.app.workspace.getActiveViewOfType(obsidian.MarkdownView);
-                            if (!view){return;};
-                            const  cmEditor = view.editor;
                             const isNative = !this.settings.twitterEmojiActive;
-                            const myModal = new EmojiModal(this.app, theme, isNative,cmEditor);
+                            const view = this.app.workspace.getActiveViewOfType(obsidian.MarkdownView);
+                            if (!view) {
+                                return;
+                            }
+                            const myModal = new EmojiModal(this.app, theme, isNative, view.editor);
                             myModal.open();
                             document.getElementsByClassName("emoji-mart-search")[0].getElementsByTagName('input')[0].focus();
                             document.getElementsByClassName("emoji-mart-search")[0].getElementsByTagName('input')[0].select();
